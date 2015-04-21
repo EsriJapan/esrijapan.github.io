e25fed7b52d05cc04840edb6959b44d8f873120e
ynunokawa <yusuke_nunokawa@esrij.com>
2015-04-22 08:53:54 +0900

    fix filterling

--- js/getrepo.js
+++ js/getrepo.js
@@ -31,87 +31,90 @@ $(function () {
         	// リポジトリ名⇒フィルタリング用のクラスを作成⇒配列
         	var repo_name = repos[i].name;
         	var name_split = repo_name.split("-")
-        	for (var j = 0; j < name_split.length; j++) {
-        		// 同名のタグ候補がある場合（あるいはesrijapan.github.io）は無視
-        		var sametag = false;
-        		for(var k = 0;  k < data_filter.length; k++) {
-        			if(data_filter[k] === name_split[j]) {
-        				sametag = true;
-        			}
-							else if(name_split[j] === "esrijapan.github.io") {
-								sametag = true;
-							}
-        		}
-        		// 同名のタグ候補が無い場合
-        		if(sametag === false) {
-        			// フィルタリング用クラス配列にプッシュ
-        			data_filter.push(name_split[j]);
-        		}
-        	}
 
-        	// リポジトリの言語
-        	if(repos[i].language == 'C#' || repos[i].language == 'Objective-C' || repos[i].language == 'Java' || repos[i].language == 'JavaScript' || repos[i].language == 'HTML') {
-        		console.log(repos[i]);
-        		var repo_class;
-	        	var header_class;
-	        	var h3_class;
-	        	// フィルタリング用の追加クラスを作成
-	        	var add_class = repos[i].name.replace(/-/g, " ");
-	        	// リポジトリの言語で分岐
-	        	switch (repos[i].language){
-				  case 'C#':
-				    repo_class = dotnet_repo_class;
-				    header_class = dotnet_header_class;
-				    h3_class = dotnet_h3_class;
-				    language = '.NET';
-				    break;
-				  case 'Objective-C':
-				    repo_class = ios_repo_class;
-				    header_class = ios_header_class;
-				    h3_class = ios_h3_class;
-				    language = 'Objective-C';
-				    break;
-				  case 'Java':
-				    repo_class = android_repo_class;
-				    header_class = android_header_class;
-				    h3_class = android_h3_class;
-				    language = 'Java';
-				    break;
-				  case 'JavaScript':
-				    repo_class = js_repo_class;
-				    header_class = js_header_class;
-				    h3_class = js_h3_class;
-				    language = 'JavaScript';
-				    break;
-				  case 'HTML':
-				    repo_class = js_repo_class;
-				    header_class = js_header_class;
-				    h3_class = js_h3_class;
-				    language = 'JavaScript';
-				    break;
-				}
-				// フィルタリング用のクラスを追加
-				repo_class = repo_class + " " + add_class;
+        	// esrijapan.github.io は非表示
+        	if(repo_name !== "esrijapan.github.io") {
 
-		        // タグの生成
-		        var repo_container_div = $('<div/>').addClass(repo_class);
-		        var repo_div = $('<div/>').addClass('repo');
-		        var repo_header_div = $('<div/>').addClass(header_class);
-		        var repo_title_h2 = $('<h2/>').addClass('repo-title');
-		        var repo_link_a = $('<a/>').attr("href", repos[i].html_url).text(repos[i].name);
-		        var repo_language_h3 = $('<h3/>').addClass(h3_class).text(language);
-		        var repo_description = $('<p/>').addClass('repo-description').text(repos[i].description);
+	        	for (var j = 0; j < name_split.length; j++) {
+	        		// 同名のタグ候補がある場合（あるいはesrijapan.github.io）は無視
+	        		var sametag = false;
+	        		for(var k = 0;  k < data_filter.length; k++) {
+	        			if(data_filter[k] === name_split[j]) {
+	        				sametag = true;
+	        			}
+	        		}
+	        		// 同名のタグ候補が無い場合
+	        		if(sametag === false) {
+	        			// フィルタリング用クラス配列にプッシュ
+	        			data_filter.push(name_split[j]);
+	        		}
+	        	}
 
-		        repo_title_h2.append(repo_link_a);
-		        repo_header_div.append(repo_title_h2);
-		        repo_header_div.append(repo_language_h3);
-		        repo_div.append(repo_header_div);
-		        repo_div.append(repo_description);
-		        repo_container_div.append(repo_div);
+	        	// リポジトリの言語
+	        	if(repos[i].language == 'C#' || repos[i].language == 'Objective-C' || repos[i].language == 'Java' || repos[i].language == 'JavaScript' || repos[i].language == 'HTML') {
+	        		console.log(repos[i]);
+	        		var repo_class;
+		        	var header_class;
+		        	var h3_class;
+		        	// フィルタリング用の追加クラスを作成
+		        	var add_class = repos[i].name.replace(/-/g, " ");
+		        	// リポジトリの言語で分岐
+		        	switch (repos[i].language){
+					  case 'C#':
+					    repo_class = dotnet_repo_class;
+					    header_class = dotnet_header_class;
+					    h3_class = dotnet_h3_class;
+					    language = '.NET';
+					    break;
+					  case 'Objective-C':
+					    repo_class = ios_repo_class;
+					    header_class = ios_header_class;
+					    h3_class = ios_h3_class;
+					    language = 'Objective-C';
+					    break;
+					  case 'Java':
+					    repo_class = android_repo_class;
+					    header_class = android_header_class;
+					    h3_class = android_h3_class;
+					    language = 'Java';
+					    break;
+					  case 'JavaScript':
+					    repo_class = js_repo_class;
+					    header_class = js_header_class;
+					    h3_class = js_h3_class;
+					    language = 'JavaScript';
+					    break;
+					  case 'HTML':
+					    repo_class = js_repo_class;
+					    header_class = js_header_class;
+					    h3_class = js_h3_class;
+					    language = 'JavaScript';
+					    break;
+					}
+					// フィルタリング用のクラスを追加
+					repo_class = repo_class + " " + add_class;
 
-		        content_div.append(repo_container_div);
-		        //console.log($('div#content.isotope'));
-        	}
+			        // タグの生成
+			        var repo_container_div = $('<div/>').addClass(repo_class);
+			        var repo_div = $('<div/>').addClass('repo');
+			        var repo_header_div = $('<div/>').addClass(header_class);
+			        var repo_title_h2 = $('<h2/>').addClass('repo-title');
+			        var repo_link_a = $('<a/>').attr("href", repos[i].html_url).text(repos[i].name);
+			        var repo_language_h3 = $('<h3/>').addClass(h3_class).text(language);
+			        var repo_description = $('<p/>').addClass('repo-description').text(repos[i].description);
+
+			        repo_title_h2.append(repo_link_a);
+			        repo_header_div.append(repo_title_h2);
+			        repo_header_div.append(repo_language_h3);
+			        repo_div.append(repo_header_div);
+			        repo_div.append(repo_description);
+			        repo_container_div.append(repo_div);
+
+			        content_div.append(repo_container_div);
+			        //console.log($('div#content.isotope'));
+	        	}
+
+	        }
         };
 
         // タグの登録（リポジトリ カード）
